e60efadd50fb7d7b7f36bcb1fc02c840319ec447
EmperorMirror <chrisdiamonds322@gmail.com>
2025-07-06 16:57:13 +0100

    Update StaticArray.java

--- StaticArray.java
+++ StaticArray.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class StaticArray {
+    public static void main(String[] args) {
+        // Declare a static array of size 10
+        int[] arr = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        // Populate the array with user input
+        System.out.println("Enter 10 integers:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Print all values in the array
+        System.out.println("You entered:");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
